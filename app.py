719f0bb143b62e5c1931dde93318e799d98b08dc
Tomáš Schmidl <schmidltomas@gmail.com>
2020-07-17 10:17:31 +0200

    Refactored positions and layouts of widgets in .kv file

--- app.py
+++ app.py
@@ -60,6 +60,8 @@ class MainApp(App):
 	# Config.set('graphics', 'fullscreen', 1)
 	Config.set('graphics', 'width', '1920')
 	Config.set('graphics', 'height', '1080')
+	# Config.set('graphics', 'width', '840')
+	# Config.set('graphics', 'height', '400')
 
 	def build(self):
 		layout = CustomLayout()
@@ -71,6 +73,7 @@ class MainApp(App):
 
 		# TODO redundant API call
 		Clock.schedule_once(layout.ids.temperature.update, 1)
+		Clock.schedule_once(layout.ids.temperature_shadow.update, 1)
 		Clock.schedule_once(layout.ids.temperature_icon.fetch_image, 1)
 
 		return layout
